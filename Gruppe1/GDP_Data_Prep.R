3a031cc0631eb320754b6008ee4ec25d7968fce3
nilsfred <47068219+nilsfred@users.noreply.github.com>
2020-05-07 15:44:48 +0200

    Add files via upload

--- Gruppe1/GDP_Data_Prep.R
+++ Gruppe1/GDP_Data_Prep.R
@@ -17,14 +17,6 @@ df <- df[relevantVar]
 time <- c("31.03.2017", "30.06.2017", "30.09.2017", "31.12.2017", "31.03.2018", "30.06.2018", "30.09.2018", "31.12.2018", "31.03.2019", "30.06.2019", "30.09.2019", "31.12.2019")
 time <- as.Date(time, format = "%d.%m.%Y")
 
-
-# red states
-# Auswahl festlegen
-red_states <- sort(c("Texas", "Alabama", "Oklahoma", "Kansas", "Arkansas"))
-blue_states <- sort(c("California", "New York", "Massachusetts", "Illinois","New Jersey"))
-swing_states <- sort(c("Florida", "Nevada", "Ohio", "Michigan", "Pennsylvania"))
-
-
 # Subset "blue states"
 blue_GDP_df <- GDP_data_prep(blue_states)
 
@@ -34,5 +26,18 @@ red_GDP_df <- GDP_data_prep(red_states)
 # Subset "swing states"
 swing_GDP_df <- GDP_data_prep(swing_states)
 
-all_states <- merge(merge(blue_GDP_df, red_GDP_df), swing_GDP_df)
-test <- merge(red_GDP_df, temp)
+temp1 <- merge(red_GDP_df, merge(blue_GDP_df, swing_GDP_df))
+
+
+#Grafiken
+temp3 <- merge(temp1, temp2)
+temp4 <- names(temp3)
+temp4 <- temp4[2:31]
+f <- temp3[temp4]
+f <- cor(f)[16:30,1:15]
+state_cor <-diag(f)
+attribute <-  c("red","red","red","red","red","blue","blue","blue","blue","blue", "swing","swing","swing","swing","swing")
+state_cor_df <- data_frame(all_states, state_cor, attribute)
+names(state_cor_df) <- c("State", "Cor", "Group")
+state_cor_df$Group <- factor(state_cor_df$Group, levels = c('red', 'blue', 'swing'))
+ggplot(data = state_cor_df, aes(x = all_states, y = state_cor, fill = Group)) + geom_bar(stat= "identity", position = "dodge") + scale_fill_manual("Legend", values = c("red" = "darkred", "blue" = "steelblue", "swing" = "purple"))
